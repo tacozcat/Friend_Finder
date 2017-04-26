b3c4d531f6293701b533bcf52fa85b7307f74aae
Emily Adams <tacozcat@Emilys-MacBook-Air.local>
2017-04-25 20:42:38 -0500

    not quite functional yet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,11 +3,13 @@ var bodyParser = require("body-parser");
 var friendsArray = require("../data/friends.js");
 
 
+
 module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friendsArray);
     });
     app.post("/api/friends", function(req, res){
+        friendsArray.push(req.body);
         res.json(friendsArray);
     });
 }
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,6 +10,10 @@ module.exports = function(app){
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     })
 
+        app.get('/api/friends', function(req, res){
+        res.sendFile(path.join(__dirname + "api/friends"));
+        })
+
     app.use( function(req, res){
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     })
